b1ce8cc919417c1bb7b162f8f6549e55532b9293
truonghh99 <truonghh@beloit.edu>
2020-04-03 15:22:06 -0700

    add neural network approach

--- neural_network.py
+++ neural_network.py
@@ -3,16 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from mpl_toolkits import mplot3d
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score 
 from sklearn.metrics import classification_report    
 from sklearn import tree
 from sklearn.metrics import confusion_matrix 
-from sklearn.externals.six import StringIO  
 from IPython.display import Image  
 import pydotplus
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
+from sklearn.neural_network import MLPClassifier
 
 path_1 = 'dataset/train.csv'
 path_2 = 'dataset/test.csv'
@@ -30,7 +28,7 @@ x_train = train.iloc[:,0:562]
 y_train = train['Activity']
 x_test = test.iloc[:,0:562]
 y_test = test['Activity']
-clf = ExtraTreesClassifier()
+clf = MLPClassifier()
 
 clf = clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
@@ -39,4 +37,3 @@ print("Confusion matrix on training set: ")
 print(confusion_matrix(y_train, clf.predict(x_train)))
 print("Confusion matrix on testing set: ")
 print(confusion_matrix(y_test, y_pred))
-
--- svm.py
+++ svm.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.metrics import accuracy_score 
 from sklearn import svm
+from sklearn.metrics import confusion_matrix 
+
 
 path_1 = 'dataset/train.csv'
 path_2 = 'dataset/test.csv'
@@ -27,3 +29,7 @@ clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Confusion matrix on training set: ")
+print(confusion_matrix(y_train, clf.predict(x_train)))
+print("Confusion matrix on testing set: ")
+print(confusion_matrix(y_test, y_pred))
